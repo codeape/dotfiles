3ebaf2a74ca57c44a4718be88c0574ff492f2404
Oscar Norlander <oscar-no@codeape.org>
2015-07-14 23:09:36 +0200

    Fixed PS1 bug
     * The prompt did not work with Gnome terminal.

--- lib/l33tprompt.sh
+++ lib/l33tprompt.sh
@@ -4,7 +4,7 @@ function parse_git_branch {
 	git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
 }
 
-export PS1="$(echo -n -e '\xE2\x94\x8F''\xE2\x94\x81')[\[\e[32m\]\t\[\e[0m\]]\[\e[34m\]\`git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'\`\[\e[0m\]\n$(echo -n -e '\xE2\x94\xa3''\xE2\x94\x81')[\[\e[36m\w\[\e[0m]\n$(echo -n -e '\xE2\x94\x97''\xE2\x94\x81')>\$ "
+export PS1="$(echo -n -e '\xE2\x94\x8F''\xE2\x94\x81')[\[\e[32m\]\t\[\e[0m\]]\[\e[34m\]\`git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'\`\[\e[0m\]\n$(echo -n -e '\xE2\x94\xa3''\xE2\x94\x81')[\[\e[36m\w\n\[\e[0m\]$(echo -n -e '\xE2\x94\x97''\xE2\x94\x81')>\$ "
 
 alias simpleps='export PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\u@\h:\w\$"'
 
