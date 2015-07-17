ca4733beb5c9a6042c70d8c75f5abf55aac14735
Oscar Norlander <oscar-no@codeape.org>
2015-07-17 23:29:14 +0200

    Install ubuntu 14.04 LTS
     * Added a script to easily install a new ubuntu 14.04 machine.

---
+++ install/ubuntu-14.04-LTS.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo apt-get install mesa-utils
+sudo apt-get install lvm2
+sudo apt-get install git
+sudo apt-get install vim-gui-common vim-runtime 
+sudo apt-get install ttf-dejavu
+sudo apt-get install keepassx
+# Steam
+sudo apt-get install libgl1-mesa-glx-lts-utopic:i386
+sudo apt-get install dnsmasq
+# Leadwerks
+sudo apt-get install codeblocks
+sudo apt-get install g++
+sudo apt-get install libglu1-mesa-dev
+sudo apt-get install  libopenal-dev
+
